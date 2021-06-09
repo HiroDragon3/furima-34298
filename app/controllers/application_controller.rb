a331d5a815a2948d0854e77f6c88a76e229ad19e
HiroDragon3 <hiroki.d6@gmail.com>
2021-06-09 17:11:35 +0900

    ユーザー管理機能　修正1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :password, :last_name, :first_name, :last_name_kana,
-                                             :first_name_kana, :birth_date])
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  validates :email,               presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
-  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana,      presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
-  validates :first_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date,          presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :email,               uniqueness: true
+    validates :password,            length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } # Passwordが英数字混合の正規表現
+    validates :last_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/ } # ユーザー本名全角の正規表現
+    validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana,      format: { with: /\A[ァ-ヶー－]+\z/ } # フリガナ全角の正規表現
+    validates :first_name_kana,     format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_date
+  end
 end
