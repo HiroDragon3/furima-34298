f993c2765663fc45bbbeb3cde018c2a43b9c5b69
HiroDragon3 <hiroki.d6@gmail.com>
2021-06-12 20:08:02 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
 
   def index
-    @items = Item.all
+    @item = Item.new
   end
 
   def new
     @items = Item.new
+  end
+
+  private
+
+  def item_params
+    params.requrire(:item).permit(:image).merge(user_id: current_user.id)
   end
 end
