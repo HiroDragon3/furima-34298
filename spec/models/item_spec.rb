e6701f93d729b6ecb759ad1a94e70d7c9c3b579c
HiroDragon3 <hiroki.d6@gmail.com>
2021-06-23 17:45:35 +0900

    商品出品機能　修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -42,6 +42,10 @@ RSpec.describe Item, type: :model do
         @item.item_price = '300'
         expect(@item).to be_valid
       end
+      it '販売価格は半角数字のみ保存される' do
+        @item.item_price = '10000'
+        expect(@item).to be_valid
+      end
     end
 
     context "投稿が保存できない時" do
@@ -120,10 +124,15 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Item price Out of setting range")
       end
-      it '販売価格は半角数字のみ保存可能であること' do
-        @item.item_price = '１００００'
+      it '販売価格は半角英数混合では登録できない' do
+        @item.item_price = '123１２３'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item price Half-width number", "Item price Out of setting range")
+        expect(@item.errors.full_messages).to include("Item price Half-width number")
+      end
+      it '販売価格は半角英語だけでは登録できない' do
+        @item.item_price = 'abcdef'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price Half-width number")
       end
     end
   end
