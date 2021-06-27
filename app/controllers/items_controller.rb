8cf75388215e0536e2ed1372060f3a5dcc2c09c3
HiroDragon3 <hiroki.d6@gmail.com>
2021-06-28 00:40:47 +0900

    Revert "商品情報編集機能　修正1"
    This reverts commit 541835c8398b0e837fd89e3211f0de63b6819948.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,6 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
