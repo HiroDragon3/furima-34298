a248729c69edfcae4ec97439c07af06b0e37a4c8
HiroDragon3 <hiroki.d6@gmail.com>
2021-06-28 02:09:49 +0900

    商品情報編集機能　ログインページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_signed_in, only: [:edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -40,5 +41,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
